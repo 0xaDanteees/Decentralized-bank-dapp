1f118eb37c65629e46384288524677803979bfa7
0xaDanteees <105533299+0xaDanteees@users.noreply.github.com>
2024-04-23 13:15:23 -0600

    Next 14.1 created + constants set

---
+++ bank/utils/contstants.ts
@@ -0,0 +1,93 @@
+import { createThirdwebClient, defineChain, getContract } from "thirdweb";
+ 
+const CLIENT_ID= process.env.NEXT_PUBLIC_CLIENT_ID;
+
+const client = createThirdwebClient({
+  clientId: CLIENT_ID as string,
+});
+
+const DeplymentChain = defineChain(84532);
+
+const contractAddress="0x51E31B140412651c3a8d43029e30fBaF16589650"
+
+const contractABI=[
+  {
+    "type": "fallback",
+    "name": "",
+    "inputs": [],
+    "outputs": [],
+    "stateMutability": "payable"
+  },
+  {
+    "type": "function",
+    "name": "deposit",
+    "inputs": [
+      {
+        "type": "address",
+        "name": "_receiver",
+        "internalType": "address"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "payable"
+  },
+  {
+    "type": "function",
+    "name": "getBalance",
+    "inputs": [],
+    "outputs": [
+      {
+        "type": "uint256",
+        "name": "",
+        "internalType": "uint256"
+      }
+    ],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "transfer",
+    "inputs": [
+      {
+        "type": "uint256",
+        "name": "_amount",
+        "internalType": "uint256"
+      },
+      {
+        "type": "address",
+        "name": "_receiver",
+        "internalType": "address"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "withdraw",
+    "inputs": [
+      {
+        "type": "uint256",
+        "name": "_amount",
+        "internalType": "uint256"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "receive",
+    "name": "",
+    "inputs": [],
+    "outputs": [],
+    "stateMutability": "payable"
+  }
+] as const
+
+const contract = getContract({
+  client,: client,
+  chain: DeplymentChain,
+  address: contractAddress,
+  abi: contractABI,
+});
+
