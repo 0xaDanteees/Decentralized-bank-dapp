5120d30435e56810d77f63e6c46bf635aa219b6b
0xaDanteees <105533299+0xaDanteees@users.noreply.github.com>
2024-04-24 10:56:20 -0600

    Deposit feature

--- bank/components/banking/Banking.tsx
+++ bank/components/banking/Banking.tsx
@@ -1,13 +1,12 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import { CONTRACT } from "@/utils/constants";
-import { TransactionButton, useActiveAccount, useReadContract } from "thirdweb/react";
+import { TransactionButton, useActiveAccount, useReadContract, useSendTransaction } from "thirdweb/react";
 import { prepareContractCall, toEther, toWei } from 'thirdweb';
 import { toast } from 'sonner';
 import { Loader, XIcon } from 'lucide-react';
 import { Button } from '../ui/button';
 import { Input } from '../ui/input';
-import { Separator } from '../ui/separator';
 
 export const Banking: React.FC = () => {
     
@@ -16,8 +15,10 @@ export const Banking: React.FC = () => {
     const [isDepositing, setIsDepositing]=useState(false);
     const [isWithdrawing, setIsWithdrawing]=useState(false);
     const [withdrawAmount, setWidrawAmount]= useState(0);
+    const [depositAmount, setDepositAmount]= useState(0);
+    const [receiver, setReceiver]=useState("");
 
-    const { data:balance, isLoading, refetch: refetchBalance } = useReadContract({
+    const { data:balance, isLoading, refetch: refetchBalance, isError: withdrawError } = useReadContract({
         contract: CONTRACT,
         method: "balances",
         params: [account?.address as string],
@@ -25,7 +26,7 @@ export const Banking: React.FC = () => {
             enabled: !!account,
         }
     });
-    console.log("balance",balance)
+    //console.log("balance",balance)
     let accountBalance="";
     if(balance!==undefined && balance!==null){
         accountBalance=`${toEther(balance!).toString()} ETH`
@@ -42,7 +43,8 @@ export const Banking: React.FC = () => {
     const refetchData=()=>{
         refetchBalance();
     };
-    console.log("amount", withdrawAmount)
+    //console.log("amount", withdrawAmount)
+    console.log("addy:", receiver)
     return (
         <div className="text-center py-4 mt-3 font-extrabold bg-secondary rounded-md">
             <h1 className="text-xl">Balance</h1>
@@ -124,9 +126,91 @@ export const Banking: React.FC = () => {
                                         setIsWithdrawing(false);
                                         toast.success("Tx confirmed.")
                                     }}
-                                    onError={()=>toast.warning(`Tx failed: ${window.console.error}`.slice(0,80))}
+                                    onError={()=>toast.warning(`Tx failed: ${withdrawError}`.slice(0,40))}
                                 >
                                     Withdraw
+                                </TransactionButton>
+                            </Button>
+                        </div>
+                    </div>
+                </>
+            )}
+
+            {isDepositing && (
+                <>
+                    <div style={{
+                            position: "fixed",
+                            top: 0,
+                            left: 0,
+                            width: "100%",
+                            height: "100%",
+                            backgroundColor: "rgba(0, 0, 0, 0.5)",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}>
+                            <div style={{
+                                position: "relative",
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center",
+                                backgroundColor: "background",
+                                padding: "40px",
+                                borderRadius: "10px",
+                                minWidth: "500px",
+                            }}>
+                            <Button
+                                className="absolute top-2 right-2 px-3 py-1 rounded-md"
+                                onClick={() => setIsDepositing(false)}
+                                size="sm"
+                                variant="ghost"
+                            >
+                                <XIcon className='text-xl text-red-600 w-full h-full'/>
+                            </Button>
+                            <h3 className="text-medium">Deposit now</h3>
+                            <p className='mt-6 mb-1 mr-auto'>To</p>
+                            <Input
+                                type="string"
+                                
+                                placeholder='0x address'
+                                value={receiver}
+                                onChange={(event)=>setReceiver(event.target.value)}
+                                className='mb-3'
+                            />
+                            <p className='mt-6 mb-1 mr-auto'>Amount</p>
+                            <Input
+                                type="number"
+                                min="0"
+                                placeholder='0.000 ETH'
+                                value={depositAmount}
+                                onChange={(event)=>setDepositAmount(parseFloat(event.target.value))}
+                                className=' mb-3'
+                            />
+                            <Button 
+                                asChild
+                                className='flex font-medium' 
+                                variant="outline"   
+                            >
+                                <TransactionButton
+                                    transaction={()=>(
+                                        prepareContractCall({
+                                            contract: CONTRACT,
+                                            method: "deposit",
+                                            params: [receiver],
+                                            value: toWei(depositAmount.toString())
+                                        })
+                                    )}
+                                    onClick={()=>toast.message("sending Tx...")}
+                                    onTransactionSent={()=>toast.info("Tx sent...")}
+                                    onTransactionConfirmed={()=>{
+                                        setWidrawAmount(0);
+                                        refetchData();
+                                        setIsDepositing(false);
+                                        toast.success("Tx confirmed.")
+                                    }}
+                                    onError={()=>toast.warning(`Tx failed: ${window.console.error}`.slice(0,80))}
+                                >
+                                    Deposit
                                 </TransactionButton>
                             </Button>
                         </div>
