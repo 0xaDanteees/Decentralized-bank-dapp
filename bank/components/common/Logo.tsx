2c0dd632e4857dbf5b2d92be7af2ce3d6695867d
0xaDanteees <105533299+0xaDanteees@users.noreply.github.com>
2024-04-24 19:11:59 -0600

    Logo src typo

--- bank/components/common/Logo.tsx
+++ bank/components/common/Logo.tsx
@@ -12,7 +12,7 @@ export const Logo =()=>{
     return(
         <div className="hidden md:flex items-center gap-x-2">
             <Image
-            src="/oriasLogo.png"
+            src="/OriasLogo.png"
             height={40}
             width={40}
             alt="Logo"
